6a8362d9a6dc41e7a165122d0f26ee6ff8609589
web2ls <web2ls@yandex.ru>
2019-11-01 14:39:22 +0300

    Fetching data via local proxy node

--- platform/viewer/src/assignmentList/AssignmentList.js
+++ platform/viewer/src/assignmentList/AssignmentList.js
@@ -1,9 +1,10 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './AssignmentList.css';
 
 export default class AssignmentList extends Component {
   constructor(props) {
     super(props);
+    this.state = { assignments: [] };
   }
 
   componentDidMount() {
@@ -12,13 +13,27 @@ export default class AssignmentList extends Component {
     const url = `http://localhost:4000/assignment/list`;
     fetch(url)
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => {
+        console.log(data);
+        this.setState({ assignments: data.assignments });
+      })
       .catch(error => console.log(error));
   }
 
   render() {
+    const assignmentList = this.state.assignments.map((item, index) => (
+      <li key={index} className="assign-item">
+        <div>{item.patient}</div>
+        <div>{item.modality}</div>
+        <div>{item.status}</div>
+      </li>
+    ))
+
     return (
-      <div className="assignment-list">Test markup</div>
-    )
+      <div className="assignment-list">
+        <h3>Assignment List</h3>
+        <ul className="assign-list">{assignmentList}</ul>
+      </div>
+    );
   }
 }
